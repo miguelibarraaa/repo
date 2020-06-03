d03413772a849b16542ff33774b96b5855126ccb
miguel <quierovervideosporfa@gmail.com>
2020-06-03 14:19:07 +0200

    3r comit integrate master

---
+++ public class covid 19.java
@@ -0,0 +1,15 @@
+public class covid 19{
+
+    private int quarentena = 15;
+    private boolean test;
+    private boolean positiu;
+
+    if(test = true) {
+        positiu = true
+    }else (positiu = false){
+        System.out.println("no tens corona virus");
+    }
+
+
+
+}
